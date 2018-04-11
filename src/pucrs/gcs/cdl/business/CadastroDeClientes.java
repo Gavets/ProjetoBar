3020e53c8825e68b8745bfa2ca4601474bc6bbcb
Larissa Salerno <larissa.salerno@acad.pucrs.br>
2018-04-11 18:53:29 -0300

    Add files via upload
    esqueleto da classe

---
+++ src/pucrs/gcs/cdl/business/CadastroDeClientes.java
@@ -0,0 +1,95 @@
+
+public class CadastroDeClientes extends Cliente {
+
+	
+	private Cliente[] cliente;
+	private int quant;
+	
+	
+	public CadastroDeClientes(int quant) {
+		
+		this.quant = quant;
+		
+	}
+	
+	public void CadastraCliente() {
+		
+	}
+	
+	public void removeCliente(){
+		
+	}
+	
+	public void alteraCliente() {
+		
+	}
+	
+	public Cliente[] consultaCliente(){
+		
+	}
+	
+	public Cliente ConsultaPorCpf(){
+		
+	}
+	
+	public double consultaPorcentagemGenero(){
+		
+	}
+	
+	public double consultaPorcentagemSocios(){
+		
+	}
+	
+	public void exportaClientes(){
+		
+	}
+	
+}
+
+    public class CadastroDeClientes extends Cliente {
+
+	
+	private Cliente[] cliente;
+	private int quant;
+	
+	
+	public CadastroDeClientes(int quant) {
+		
+		this.quant = quant;
+		
+	}
+	
+	public void CadastraCliente() {
+		
+	}
+	
+	public void removeCliente(){
+		
+	}
+	
+	public void alteraCliente() {
+		
+	}
+	
+	public Cliente[] consultaCliente(){
+		
+	}
+	
+	public Cliente ConsultaPorCpf(){
+		
+	}
+	
+	public double consultaPorcentagemGenero(){
+		
+	}
+	
+	public double consultaPorcentagemSocios(){
+		
+	}
+	
+	public void exportaClientes(){
+		
+	}
+	
+}
+
